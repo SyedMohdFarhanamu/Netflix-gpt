39bfb2b64d6c6051d1a21a0471048bc8afe50fa1
SyedMohdFarhanamu <syedfarhan.amu@gmail.com>
2025-05-01 01:20:05 +0530

    signIn/signUp && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App text-3xl font-bold text-green-700">
-     Namaste everyone let's build Namaste-GPT.
-    </div>
+    <Body/>
   );
 }
 
